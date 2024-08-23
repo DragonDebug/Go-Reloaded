3e30f76a4756dd585ca06b0cb48133b9bfcb0d20
Reboot01 <random>
2024-08-23 18:15:05 +0000

    updates

--- main/main.go
+++ main/main.go
@@ -5,7 +5,6 @@ import (
 	fn "goreloaded"
 	"os"
 	re "regexp"
-	"strings"
 )
 
 func main() {
@@ -36,43 +35,37 @@ func main() {
 
 	// Converting the Data to a readable form and spliting it
 	content := string(data)
-	newContent := content
 
 	// Regexp command for finding the required patterns
 	pattern := `(?i)((?:\w+\s*)+)(?:[.,;:!?])*\s*\(\s*(cap|up|low|bin|hex)\s*(?:,\s*(\d+)\s*)?\)`
-	re := re.MustCompile(pattern)
-
-	// Find all matches
-	matches := re.FindAllStringSubmatch(content, -1)
+	reMain := re.MustCompile(pattern)
 
 	// Iterate through the matches and replace them with the appropriate modifications
-	for _, match := range matches {
-		sentence := match[1]
-		command := match[2]
-		count := 1 // default count is 1 if no number is provided
-		if match[3] != "" {
-			count = fn.StringToInt(match[3])
+	newContent := reMain.ReplaceAllStringFunc(content, func(match string) string {
+		submatches := reMain.FindStringSubmatch(match)
+		if len(submatches) >= 3 {
+			sentence := submatches[1]
+			command := submatches[2]
+			count := 1 // default count is 1 if no number is provided
+			if submatches[3] != "" {
+				count = fn.StringToInt(submatches[3])
+			}
+
+			// Check the command and make the correct modifications
+			if fn.IsCap(command) {
+				return fn.TurnCapital(sentence, count)
+			} else if fn.IsUp(command) {
+				return fn.TurnUpper(sentence, count)
+			} else if fn.IsLow(command) {
+				return fn.TurnLower(sentence, count)
+			} else if fn.IsBin(command) {
+				return fn.TurnBinToDec(sentence)
+			} else if fn.IsHex(command) {
+				return fn.TurnHexToDec(sentence)
+			}
 		}
-
-		// Replace based on the command
-		var replacement string
-
-		if fn.IsCap(command) {
-			replacement = fn.TurnCapital(sentence, count)
-		} else if fn.IsUp(command) {
-			replacement = fn.TurnUpper(sentence, count)
-		} else if fn.IsLow(command) {
-			replacement = fn.TurnLower(sentence, count)
-		} else if fn.IsBin(command) {
-			replacement = fn.TurnBinToDec(sentence)
-		} else if fn.IsHex(command) {
-			replacement = fn.TurnHexToDec(sentence)
-		}
-
-		// Replace the content with the modified word
-		newContent = strings.Replace(newContent, match[0], replacement, 1)
-
-	}
+		return match // Return original if not matched correctly
+	})
 
 	// Cleaning the spaces before the special characters and points
 	newContent = fn.CleanSpecial(newContent)
